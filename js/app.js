85a298ad065adac56bebaf45934128a1ea8452c7
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2026-02-11 16:58:07 -0300

    css e html terminado

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const main = document.querySelector('main')
+const sectionCursos = document.getElementById('Cursos')
+const sectionGalerias = document.getElementById('Galeria-Alunos')
+const sectionAluno = document.getElementById('studant')
+
+async function exibirTodosCursos() {
+    
+}
+async function exibirTodosEstudantes() {
+    
+}
+async function exibirEstudantesPorCurso(idCurso) {
+    
+}
+async function exibirDadosEstudante(idAluno) {
+    
+}
+
