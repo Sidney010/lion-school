ae5e5feba2ca0012f94cfb48212c4b616531976b
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2026-02-24 17:01:12 -0300

    FRONT

--- js/app.js
+++ js/app.js
@@ -4,17 +4,165 @@ const main = document.querySelector('main')
 const sectionCursos = document.getElementById('Cursos')
 const sectionGalerias = document.getElementById('Galeria-Alunos')
 const sectionAluno = document.getElementById('studant')
+const containerCursos = document.querySelector('.Right-ClassListAvailable')
+const containerAlunos = document.getElementById('id-classe')
+const tituloCurso = document.getElementById('title')
+const pathImg = `./img`
+const pathImgIcons = `/icons-class`
+
+const URL_BASE = "https://lion-school-phbo.onrender.com/"
 
 async function exibirTodosCursos() {
-    
-}
-async function exibirTodosEstudantes() {
-    
+    let URL_CURSOS = ` ${URL_BASE}/cursos`
+    try {
+        let response = await fetch(URL_CURSOS)
+        const cursos = await response.json()
+
+        containerCursos.textContent("")
+
+        cursos.forEach(curso => {
+            const card = document.createElement('div')
+            const titulo = document.createElement('h1')
+
+            card.classList.add('Card-Class')
+            titulo.textContent(curso.sigla)
+
+            const img = document.createElement('img')
+
+            if (curso.sigla.toUpperCase() == 'DS')
+                img.src = pathImg + pathImgIcons + `/icon-ds.svg`
+            else if (curso.sigla.toUpperCase() == 'REDES')
+                img.src = pathImg + pathImgIcons + `/icon-redes.svg`
+            else
+                return false
+
+            titulo.classList.add('Card-Class')
+            img.classList.add('Card-Class')
+
+            // card.addEventListener('click', () => {
+            //     exibirEstudantesPorCurso(curso.id, curso.nome)
+            // })
+
+            containerCursos.appendChild(card)
+
+        });
+
+    } catch (error) {
+
+    }
+
+
+    // RETORNO ESPERADO
+    // [
+    //     {
+    //       "id": 1,
+    //       "nome": "Desenvolvimento de Sistemas",
+    //       "sigla": "DS"
+    //     },
+    //     {
+    //       "id": 2,
+    //       "nome": "Redes",
+    //       "sigla": "REDES"
+    //     }
+    //   ]   
+
 }
 async function exibirEstudantesPorCurso(idCurso) {
-    
+    let URL_ESTUDANTES_CURSO = ` ${URL_BASE}/alunos?${idCurso}`
+
+    //RETORNO ESPERADO
+    // [
+    //  {
+    //     "id": 1,
+    //     "nome": "Mariana Silva Santos",
+    //     "curso_id": 1,
+    //     "foto": "https://i.pravatar.cc/300?img=25",
+    //     "desempenho": [
+    //         {
+    //             "categoria": "SGP",
+    //             "valor": 85
+    //         },
+    //         {
+    //             "categoria": "IP",
+    //             "valor": 92
+    //         },
+    //         {
+    //             "categoria": "LING",
+    //             "valor": 78
+    //         },
+    //         {
+    //             "categoria": "BD",
+    //             "valor": 88
+    //         },
+    //         {
+    //             "categoria": "PPE",
+    //             "valor": 95
+    //         }
+    //     ]
+    // },
+    // {
+    //     "id": 2,
+    //     "nome": "Lucas Ferreira Costa",
+    //     "curso_id": 1,
+    //     "foto": "https://i.pravatar.cc/300?img=2",
+    //     "desempenho": [
+    //         {
+    //             "categoria": "SGP",
+    //             "valor": 72
+    //         },
+    //         {
+    //             "categoria": "IP",
+    //             "valor": 85
+    //         },
+    //         {
+    //             "categoria": "LING",
+    //             "valor": 80
+    //         },
+    //         {
+    //             "categoria": "BD",
+    //             "valor": 90
+    //         },
+    //         {
+    //             "categoria": "PPE",
+    //             "valor": 88
+    //         }
+    //     ]
+    // },
+    // ]
+
+
 }
 async function exibirDadosEstudante(idAluno) {
-    
+    let URL_DADS_ESTUDANTES = ` ${URL_BASE}/alunos/${idAluno}`
+    // RETORNO ESPERADO
+    // {
+    //     "id": 1,
+    //     "nome": "Mariana Silva Santos",
+    //     "curso_id": 1,
+    //     "foto": "https://i.pravatar.cc/300?img=25",
+    //     "desempenho": [
+    //       {
+    //         "categoria": "SGP",
+    //         "valor": 85
+    //       },
+    //       {
+    //         "categoria": "IP",
+    //         "valor": 92
+    //       },
+    //       {
+    //         "categoria": "LING",
+    //         "valor": 78
+    //       },
+    //       {
+    //         "categoria": "BD",
+    //         "valor": 88
+    //       },
+    //       {
+    //         "categoria": "PPE",
+    //         "valor": 95
+    //       }
+    //     ]
+    //   }
+
 }
 
